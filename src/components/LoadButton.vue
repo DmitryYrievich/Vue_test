4a4f05de9b1f180ba24abddaecaa0a0bbf765037
d.pokhyla <d.pokhyla@viseven.com>
2020-10-14 22:45:00 +0300

    add(*): some crazy code

---
+++ src/components/LoadButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <button class="load-button" @click="loadAvatar">Load more</button>
+</template>
+<script>
+import { mapActions, mapGetters } from "vuex";
+
+export default {
+  computed: {
+    ...mapGetters(["oneAvatar"]),
+    ...mapGetters(["allAvatars"]),
+  },
+  async created() {
+    this.fetchOneAvatar();
+    this.fetchAvatars();
+  },
+  methods: {
+    loadAvatar() {
+      if (this.allAvatars.length < this.oneAvatar.length) {
+        this.allAvatars.push(this.oneAvatar[this.allAvatars.length]);
+      }
+    },
+    ...mapActions(["fetchOneAvatar"]),
+    ...mapActions(["fetchAvatars"]),
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
